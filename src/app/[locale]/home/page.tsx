7618ab48410eac08dbb967502c2ed7d0a8c811da
Bao <baoohg@gmail.com>
2023-12-28 20:32:08 +0700

    add card, add carousel

--- src/app/[locale]/home/page.tsx
+++ src/app/[locale]/home/page.tsx
@@ -11,6 +11,8 @@ import Modal from "@/components/modal";
 import Hamburger from "@/components/hamburger";
 import Avatar from "@/components/avatar";
 import Badge from "@/components/badge";
+import Card from "@/components/card";
+import Carousel from "@/components/carousel"
 
 export default function Root() {
   const t = useTranslations("Index");
@@ -64,7 +66,32 @@ export default function Root() {
           console.log("Hello");
         }}
       /> */}
-      <Badge title="hehe" />
+      {/* <Badge title="hehe" /> */}
+      <Card 
+        actions="Fight!" 
+        items=
+        {[{
+          url: "https://ih1.redbubble.net/image.2506552510.6595/st,small,507x507-pad,600x600,f8f8f8.u1.jpg",
+          alt: "Eula",
+          title: "Eula",
+          description: "Eula!",
+          handleActionClick: () => console.log("1")
+        },
+        {
+          url: "https://ih1.redbubble.net/image.2506552510.6595/st,small,507x507-pad,600x600,f8f8f8.u1.jpg",
+          alt: "Eula",
+          title: "Eula",
+          description: "Eula!",
+          handleActionClick: () => console.log("2")
+        }]}/>
+      {/* <Carousel img={[
+        {url: "https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg", alt:"1"}
+      ,
+      {url: "https://daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg", alt: "2"},
+      {url:"https://daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg", alt: "3"},
+      {url:"https://daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg", alt: "4"}, {
+        url:"https://daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg", alt: "5" 
+      }]}/> */}
     </div>
   );
 }
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,21 +1,29 @@
-const Card = () => {
+const Card = (props: any) => {
+  const { items = [], actions } = props;
+  // const handleActionClick = () => {
+  //   handleAction();
+  // }
+
   return (
     <>
-      <div className="card w-96 bg-base-100 shadow-xl">
+      {items.map((item : any) => (
+      <div className="h-1/3 w-1/3 card w-96 bg-base-100 shadow-xl">
         <figure>
           <img
-            src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-            alt="Shoes"
+            src={item.url}
+            alt={item.alt}
           />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <h2 className="card-title">{item.title}</h2>
+          <p>{item.description}</p>
           <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
+            <button onClick={item.handleActionClick} className="btn btn-primary">{actions}</button>
           </div>
         </div>
-      </div>
+      </div>))}
     </>
   );
 };
+
+export default Card;
