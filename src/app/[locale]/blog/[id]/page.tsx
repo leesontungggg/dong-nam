416d23e876b16f06248574941016e112e044f927
Bao <baoohg@gmail.com>
2024-04-22 12:15:20 +0700

    fix blog-id

--- src/app/[locale]/blog/[id]/page.tsx
+++ src/app/[locale]/blog/[id]/page.tsx
@@ -20,25 +20,38 @@ export default function Root() {
       : null;
 
   return (
-    <div className="w-full min-h-screen bg-white text-black flex flex-col justify-start pt-10 items-center">
-      <div className="p-4 pb-10">
-        <div className="container mx-auto">
+    <div className="w-full min-h-screen bg-white text-black flex flex-col justify-start pt-4 items-center">
+      <div className=" pb-10">
+        <div className="mx-auto">
           {!!blog && (
-            <div className="flex flex-col items-center gap-4">
-              <h1 className="text-black font-medium text-4xl">
-                {locale === "vi" ? blog.title : blog.title_en}
-              </h1>
+            <div className="flex flex-col items-center gap-4 w-full  h-full">
               <img
                 src={`https://dongnam.up.railway.app/assets/${blog.thumbnail}`}
                 alt="DONG-NAM"
-                className="rounded bg-base-200 object-cover w-auto h-80"
+                className=" bg-base-200 object-cover w-screen h-screen rounded-lg max-h-screen"
               />
-              <div
-                dangerouslySetInnerHTML={{
-                  __html: locale === "vi" ? blog.content : blog.content_en,
-                }}
-                className="!text-black"
-              ></div>
+              <div className="container lg:max-w-[88vw] xl:max-w-[58vw]  pt-6">
+                <h1 className="text-[#0aa652] leading-[43px] font-bold text-[38px] pb-6">
+                  {locale === "vi" ? blog.title : blog.title_en}
+                </h1>
+                <div className="flex flex-row w-full justify-between pb-6 text-[22.6px]">
+                  <p className="">Jacy Huynh</p>
+                  <p className="justify-end">12.03.2024</p>
+                </div>
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html:
+                      locale === "vi" ? blog.description : blog.description_en,
+                  }}
+                  className="!text-black pb-6 text-[22.6px] font-bold text-justify"
+                ></div>
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: locale === "vi" ? blog.content : blog.content_en,
+                  }}
+                  className="!text-black  text-[22.6px] text-justify lg:leading-[34px] xl:leading-[40px]"
+                ></div>
+              </div>
             </div>
           )}
         </div>
