f8054d2feeca3c6fb6af0b58d6bb37646a4787ea
Bao <baoohg@gmail.com>
2024-04-22 22:02:50 +0700

    fix text blog/id

--- src/app/[locale]/blog/[id]/page.tsx
+++ src/app/[locale]/blog/[id]/page.tsx
@@ -30,7 +30,7 @@ export default function Root() {
                 alt="DONG-NAM"
                 className=" bg-base-200 object-cover w-screen h-screen rounded-lg max-h-screen"
               />
-              <div className="container max-w-full xl:max-w-[58vw] pt-6">
+              <div className="container max-w-full lg:px-8 xl:px-12 2xl:max-w-[58vw] pt-6">
                 <h1 className="text-[#0aa652] leading-[43px] font-bold text-[38px] pb-6">
                   {locale === "vi" ? blog.title : blog.title_en}
                 </h1>
