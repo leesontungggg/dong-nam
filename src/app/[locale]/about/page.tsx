1357405951fb1936d6a03d91c266f48aa8dfb367
Bao <baoohg@gmail.com>
2024-04-29 04:27:56 +0700

    update about-us mobile, fix footer

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -5,22 +5,33 @@ import { useRouter, usePathname } from "next-intl/client";
 import { useTransition } from "react";
 
 export default function About() {
-  const t = useTranslations("hero");
+  const t = useTranslations("about");
   const router = useRouter();
   const pathname = usePathname();
   const [isPending, startTransition] = useTransition();
   const locale = useLocale();
 
+  const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const lang = e.target.value;
+    startTransition(() => {
+      router.replace(pathname, { locale: lang });
+    });
+  };
+
   return (
     <main className="!font-sans">
-      <section className="relative bg-white min-h-screen ">
+      <section className="hidden md:block relative bg-white min-h-screen ">
         <div className="absolute top-0 left-0 w-full h-full opacity-100 bg-[url('/image/about-hero-bg.jpg')] bg-cover bg-repeat bg-cli" />
         <div className="absolute bg-transparent container flex flex-col-reverse gap-20 max-w-screen-xl mx-auto pt-[4em] lg:min-h-screen lg:items-center lg:flex-row left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2">
-          <div className="flex flex-col gap-6 content-center items-center lg:h-100 lg:w-1/2 bg-white rounded-3xl p-8">
-            <h1 className="text-3xl text-[#00A44E] font-bold text-center mx-auto lg:mx-0 load-hidden animate-fade-in">
-              {t("title")}
+          <div className="flex flex-col gap-6 content-center items-center lg:h-100 lg:w-1/2 bg-white rounded-3xl md:p-5 xl:p-8">
+            <h1 className="text-3xl text-[#00A44E] font-bold  mx-auto lg:mx-0 text-center load-hidden animate-fade-in uppercase">
+              {locale === "vi" ? (
+                <>Công ty Dược Đông Nam</>
+              ) : (
+                <>Dong Nam Pharmaceuticals</>
+              )}
             </h1>
-            <p className="text-center load-hidden animate-fade-in animate-delay-200 text-slate-950 text-2xl leading-10">
+            <p className="load-hidden animate-fade-in animate-delay-200 text-slate-950 text-2xl leading-10 text-justify">
               {locale === "vi" ? (
                 <>
                   Công ty Dược Phẩm Đông Nam được thành lập từ năm 2001, là đơn
@@ -39,17 +50,46 @@ export default function About() {
           </div>
         </div>
       </section>
-      <section className="bg-base-200 py-[3em] px-[2em] md:py-[4em] lg:p-[5em]">
+      <section className="flex flex-col md:hidden w-full">
+        <img src="/image/about-hero-bg.jpg" className="pb-4" />
+        <div className="flex flex-col w-full md:container px-7">
+          <p className="text-[18px] text-[#00A44E] font-bold pb-2 uppercase text-center">
+            {/* {locale === "vi" ? (
+              <>Công ty Dược Phẩm Đông Nam</>
+            ) : (
+              <>Dong Nam Pharmaceuticals</>
+            )} */}
+            {t("title1")}
+          </p>
+          <p className="text-justify leading-[20px] text-[16px]">
+            {locale === "vi" ? (
+              <>
+                Công ty Dược Phẩm Đông Nam được thành lập từ năm 2001, là đơn vị{" "}
+                <strong>tiên phong</strong> sản xuất dược phẩm{" "}
+                <strong>đạt tiêu chuẩn GMP-WHO</strong> tại Việt Nam.
+              </>
+            ) : (
+              <>
+                Dong Nam Pharmaceutical Company was established in 2001 and is a{" "}
+                <strong>pioneering unit</strong> in manufacturing
+                pharmaceuticals meeting <strong>GMP- WHO standards</strong> in
+                Vietnam.
+              </>
+            )}
+          </p>
+        </div>
+      </section>
+      <section className="hidden md:block bg-base-200 py-[3em] px-[2em] md:py-[4em] lg:p-[5em]">
         <div className="container max-w-screen-xl mx-auto grid grid-cols-2 gap-8">
           <img
             src="/image/about-1.jpg"
-            className="w-full h-full rounded-lg max-h-80 object-cover"
+            className="w-full h-full rounded-lg xl:max-h-80 object-cover"
           />
           <div className="flex flex-col gap-4">
             <h1 className="text-3xl uppercase font-bold text-[#F18200]">
               {locale === "vi" ? "Về chúng tôi" : "About us"}
             </h1>
-            <p className="text-2xl leading-10">
+            <p className="text-2xl leading-10 text-justify">
               {locale === "vi" ? (
                 <>
                   Với{" "}
@@ -65,22 +105,22 @@ export default function About() {
               ) : (
                 <>
                   With a team of <strong>experienced staff</strong> and a{" "}
-                  <strong>modern factory chain</strong>
-                  spanning over <strong>13,000 m2</strong>, we are a leading
-                  pharmaceutical manufacturer, continuously investing in
-                  technology and quality management to ensure that every Dong
-                  Nam branded product meets the highest standards.
+                  <strong>modern factory chain</strong> spanning over{" "}
+                  <strong>13,000 m2</strong>, we are a leading pharmaceutical
+                  manufacturer, continuously investing in technology and quality
+                  management to ensure that every Dong Nam branded product meets
+                  the highest standards.
                 </>
               )}
             </p>
           </div>
         </div>
-        <div className="container max-w-screen-xl mx-auto grid grid-cols-2 gap-8 mt-32">
+        <div className="container max-w-screen-xl mx-auto grid grid-cols-2 gap-8 mt-32 ">
           <div className="flex flex-col gap-4">
             <h1 className="text-3xl uppercase font-bold text-[#00A44E]">
               {locale === "vi" ? "Tầm nhìn" : "Our vision"}
             </h1>
-            <p className="text-2xl leading-10">
+            <p className="text-2xl leading-10 text-justify">
               {locale === "vi" ? (
                 <>
                   Tầm nhìn của chúng tôi là{" "}
@@ -98,26 +138,26 @@ export default function About() {
                     promoting progress and innovation in the industry,
                   </strong>{" "}
                   contributing to improving the quality of life for people
-                  globally
+                  globally.
                 </>
               )}
             </p>
           </div>
           <img
             src="/image/about-2.jpg"
-            className="w-full h-full rounded-lg max-h-80 object-cover"
+            className="w-full h-full rounded-lg xl:max-h-80 object-cover"
           />
         </div>
         <div className="container max-w-screen-xl mx-auto grid grid-cols-2 gap-8 mt-32">
           <img
             src="/image/about-3.jpg"
-            className="w-full h-full rounded-lg max-h-80 object-cover"
+            className="w-full h-full rounded-lg xl:max-h-80 object-cover"
           />
           <div className="flex flex-col gap-4">
             <h1 className="text-3xl uppercase font-bold text-[#F18200]">
               {locale === "vi" ? "Sứ mệnh" : "Our mission"}
             </h1>
-            <p className="text-2xl leading-10">
+            <p className="text-2xl leading-10 text-justify">
               {locale === "vi" ? (
                 <>
                   Sứ mệnh của chúng tôi không chỉ là{" "}
@@ -153,7 +193,7 @@ export default function About() {
             <h1 className="text-3xl uppercase font-bold text-[#00A44E]">
               {locale === "vi" ? "Chiến lược" : "Strategy"}
             </h1>
-            <p className="text-2xl leading-10">
+            <p className="text-2xl leading-10 text-justify">
               {locale === "vi" ? (
                 <>
                   Điểm mạnh của chúng tôi không chỉ nằm ở việc phát triển các
@@ -165,7 +205,7 @@ export default function About() {
                   Chúng tôi tự tin về khả năng sản xuất các sản phẩm chất lượng,
                   từ loại viên nén, viên nang cứng, viên nang mềm và viên nén
                   bao phim, đáp ứng mọi nhu cầu trong điều trị và bảo vệ sức
-                  khỏe,
+                  khỏe.
                 </>
               ) : (
                 <>
@@ -184,8 +224,138 @@ export default function About() {
           </div>
           <img
             src="/image/about-4.jpg"
-            className="w-full h-full rounded-lg max-h-80 object-cover"
+            className="w-full h-full rounded-lg xl:h-[372px] object-cover"
           />
+        </div>
+      </section>
+      <section className="flex flex-col md:hidden w-full ">
+        <img src="/image/about-1.jpg" className="container py-4" />
+        <div className="flex flex-col w-full md:container px-7">
+          <p className="text-[18px] text-[#F18200] font-bold pb-2 uppercase">
+            {t("title2")}
+          </p>
+          <p className="text-justify leading-[20px] text-[16px]">
+            {locale === "vi" ? (
+              <>
+                Với{" "}
+                <strong>
+                  đội ngũ nhân viên dày dặn kinh nghiệm, chuỗi nhà máy hiện đại
+                  có diện tích hơn 13,000 m2
+                </strong>
+                . chúng tôi là một nhà sản xuất dược phẩm không ngừng đầu tư vào
+                công nghệ và quản lý chất lượng để đảm bảo rằng mỗi sản phẩm
+                mang thương hiệu Đông Nam đều đáp ứng được các tiêu chuẩn cao
+                nhất.
+              </>
+            ) : (
+              <>
+                With a team of <strong>experienced staff</strong> and a{" "}
+                <strong>modern factory chain</strong> spanning over 13,000 m2,
+                we are a leading pharmaceutical manufacturer, continuously
+                investing in technology and quality management to ensure that
+                every Dong Nam branded product meets the highest standards.
+              </>
+            )}
+          </p>
+        </div>
+      </section>
+      <section className="flex flex-col md:hidden w-full ">
+        <img src="/image/about-2.jpg" className="container py-4" />
+        <div className="flex flex-col w-full md:container px-7">
+          <p className="text-[18px] text-[#00A44E] font-bold pb-2 uppercase">
+            {t("title3")}
+          </p>
+          <p className="text-justify leading-[20px] text-[16px]">
+            {locale === "vi" ? (
+              <>
+                Tầm nhìn của chúng tôi là{" "}
+                <strong>trở thành thương hiệu hàng đầu Việt Nam</strong> trong
+                lĩnh vực sản xuất dược phẩm, thông qua việc{" "}
+                <strong>thúc đẩy sự tiến bộ và đổi mới trong ngành</strong>, góp
+                phần nâng cao chất lượng cuộc sống cho người dân trên toàn cầu.
+              </>
+            ) : (
+              <>
+                Our vision is to{" "}
+                <strong>become a top brand in Vietnam's</strong> pharmaceutical
+                manufacturing sector, by{" "}
+                <strong>promoting progress and innovation</strong> in the
+                industry, contributing to improving the quality of life for
+                people globally.
+              </>
+            )}
+          </p>
+        </div>
+      </section>
+      <section className="flex flex-col md:hidden w-full ">
+        <img src="/image/about-3.jpg" className="container py-4" />
+        <div className="flex flex-col w-full md:container px-7">
+          <p className="text-[18px] text-[#F18200] font-bold pb-2 uppercase">
+            {t("title4")}
+          </p>
+          <p className="text-justify leading-[20px] text-[16px]">
+            {locale === "vi" ? (
+              <>
+                Sứ mệnh của chúng tôi không chỉ là{" "}
+                <strong>
+                  cung cấp sản phẩm chất lượng với giá cả phải chăng vì sức khỏe
+                  người Việt
+                </strong>
+                , mà còn mở rộng phạm vi tác động để cải thiện sức khỏe và cuộc
+                sống của cộng đồng quốc tế. Chúng tôi tự hào khi được tin tưởng
+                để đồng hành cùng nhiều{" "}
+                <strong>Quý khách hàng từ các quốc gia trong khu vực</strong>.
+              </>
+            ) : (
+              <>
+                Our mission is not only to{" "}
+                <strong>provide quality products at affordable prices</strong>{" "}
+                for consumers in Vietnam but also to expand our impact to
+                improve the health and lives of the international community. We
+                take pride in being trusted to accompany many{" "}
+                <strong>esteemed customers</strong> from countries in the
+                region.
+              </>
+            )}
+          </p>
+        </div>
+      </section>
+      <section className="flex flex-col md:hidden w-full ">
+        <img
+          src="/image/about-4.jpg"
+          className="container py-4 h-[276px] object-cover"
+        />
+        <div className="flex flex-col w-full md:container px-7">
+          <p className="text-[18px] text-[#00A44E] font-bold pb-2 uppercase">
+            {t("title5")}
+          </p>
+          <p className="text-justify leading-[20px] text-[16px] pb-4">
+            {locale === "vi" ? (
+              <>
+                Điểm mạnh của chúng tôi không chỉ nằm ở việc phát triển các sản
+                phẩm thuốc mới, mà còn ở khả năng{" "}
+                <strong>
+                  nghiên cứu và chế tạo sản phẩm theo yêu cầu của các Quý đối
+                  tác
+                </strong>
+                . Chúng tôi tự tin về khả năng sản xuất các sản phẩm chất lượng,
+                từ loại viên nén, viên nang cứng, viên nang mềm và viên nén bao
+                phim, đáp ứng mọi nhu cầu trong điều trị và bảo vệ sức khỏe.
+              </>
+            ) : (
+              <>
+                Our strength lies not only in developing new drug products but
+                also in our ability to research and manufacture products{" "}
+                <strong>
+                  according to the customized requirements of our valued
+                  partners
+                </strong>
+                . We are confident in our ability to produce quality products,
+                from tablets, hard capsules, and soft capsules to film-coated
+                tablets, meeting all needs in treatment and health protection.
+              </>
+            )}
+          </p>
         </div>
       </section>
     </main>
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,17 +6,17 @@ const Footer = () => {
   const locale = useLocale();
 
   return (
-    <footer className="relative flex flex-col justify-start items-start md:p-7 px-7 pt-4 bg-[#F18200] text-white">
+    <footer className="relative flex flex-col justify-start items-start md:py-4 md:px-8 px-4 pt-4 bg-[#F18200] text-white">
       <aside className="flex flex-col justify-start items-start">
-        <p className="font-bold mt-4 text-white text-center capitalize md:text-2xl xl:text-3xl">
+        <p className="font-bold md:mt-4 text-white text-justify md:text-center capitalize text-[20px] leading-[22px] md:text-2xl xl:text-3xl">
           {locale === "vi"
             ? "Công ty cổ phần sản xuất thương mại dược phẩm đông nam"
             : "Dong Nam Pharmaceutical Trading and Production Joint Stock Company"}
         </p>
-        <p className="text-white text-center mt-4 md:text-2xl xl:text-3xl">
+        <p className="text-white text-justify md:text-center md:mt-4 text-[14px] md:text-2xl xl:text-3xl">
           <img
             src="/image/footer-1.png"
-            className="w-10 h-10 inline-block mr-2"
+            className="w-10 h-10 inline-block md:mr-2"
           />
           {locale === "vi" ? (
             <>
@@ -30,10 +30,10 @@ const Footer = () => {
             </>
           )}
         </p>
-        <p className="text-white text-center md:text-2xl xl:text-3xl">
+        <p className="text-white text-justify md:text-center text-[14px] md:text-2xl xl:text-3xl">
           <img
             src="/image/footer-2.png"
-            className="w-10 h-10 inline-block mr-2"
+            className="w-10 h-10 inline-block md:mr-2"
           />
           {locale === "vi" ? (
             <>
@@ -45,15 +45,22 @@ const Footer = () => {
             </>
           )}
         </p>
+        <p className="block md:hidden text-white text-justify md:text-center text-[14px] md:text-2xl xl:text-3xl h-[16vh]">
+          <img
+            src="/image/footer-3.png"
+            className="w-10 h-10 inline-block md:mr-2"
+          />
+          <strong>Email:</strong> kinhdoanh@dongnampharma.vn
+        </p>
         <div className="text-white md:text-2xl xl:text-3xl inline    w-full">
-          <>
+          <div className="hidden md:block">
             <img
               src="/image/footer-3.png"
-              className="w-10 h-10 inline-block mr-2 "
+              className="w-10 h-10 inline-block mr-2"
             />
             <strong>Email:</strong> kinhdoanh@dongnampharma.vn
-          </>
-          <div className="absolute hidden md:flex flex-row items-center bottom-0 right-10">
+          </div>
+          <div className="absolute hidden md:flex flex-row items-center bottom-0 md:right-4 xl:right-16 2xl:right-30">
             <a
               href="https://web.facebook.com/dongnampharmaceutical"
               target="_blank"
@@ -62,18 +69,21 @@ const Footer = () => {
               <img
                 src="/image/facebook.png"
                 alt="Facebook-DongNam"
-                className="md:w-16 md:h-16 xl:h-24 xl:w-24 2xl:w-32 2xl:h-32"
+                className="md:w-16 md:h-16 xl:h-24 xl:w-24 "
               />
             </a>
             <img
               src="/image/bocongthuong.png"
               alt="DongNam"
-              className="w-auto md:h-40 xl:h-60 2xl:h-80"
+              className="w-auto md:h-40 xl:h-60 "
             />
           </div>
         </div>
       </aside>
-      <div className="md:hidden flex flex-row items-center w-full justify-center ">
+      <div
+        className="absolute bottom-0 md:hidden flex flex-row items-center justify-center"
+        style={{ width: "-webkit-fill-available" }}
+      >
         <a
           href="https://web.facebook.com/dongnampharmaceutical"
           target="_blank"
