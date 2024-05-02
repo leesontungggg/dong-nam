f9065284a0a7a99721f4b3be8cb62fbf5a7dcd53
Bao <baoohg@gmail.com>
2024-05-03 02:48:57 +0700

    fix footer, update factory

--- src/app/[locale]/factory/page.tsx
+++ src/app/[locale]/factory/page.tsx
@@ -5,22 +5,29 @@ import { useRouter, usePathname } from "next-intl/client";
 import { useTransition } from "react";
 
 export default function About() {
-  const t = useTranslations("hero");
+  const t = useTranslations("factory");
   const router = useRouter();
   const pathname = usePathname();
   const [isPending, startTransition] = useTransition();
   const locale = useLocale();
 
+  const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const lang = e.target.value;
+    startTransition(() => {
+      router.replace(pathname, { locale: lang });
+    });
+  };
+
   return (
     <main className="!font-sans">
-      <section className="relative bg-white min-h-screen ">
+      <section className="hidden md:block relative bg-white min-h-screen ">
         <div className="absolute top-0 left-0 w-full h-full opacity-100 bg-[url('/image/factory-hero-bg.jpg')] bg-cover bg-repeat bg-cli" />
         <div className="absolute bg-transparent container flex flex-col-reverse gap-20 max-w-screen-xl mx-auto pt-[4em] lg:min-h-screen lg:items-center lg:flex-row left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2">
-          <div className="flex flex-col gap-6 content-center items-center lg:h-100 lg:w-1/2 xl:w-2/5 bg-white rounded-3xl p-8">
-            <h1 className="text-xl md:text-3xl text-[#00A44E] font-bold text-center mx-auto lg:mx-0 load-hidden animate-fade-in">
+          <div className="flex flex-col gap-6 content-center items-center lg:h-100 lg:w-1/2 bg-white rounded-3xl md:p-5 xl:p-8">
+            <h1 className="text-3xl text-[#00A44E] font-bold  mx-auto lg:mx-0 text-center load-hidden animate-fade-in uppercase">
               {locale === "vi" ? <>NHÀ MÁY ĐÔNG NAM</> : <>DONG NAM FACTORY</>}
             </h1>
-            <p className="text-center load-hidden animate-fade-in animate-delay-200 text-slate-950 text-2xl">
+            <p className="load-hidden animate-fade-in animate-delay-200 text-slate-950 text-2xl leading-10 text-justify">
               {locale === "vi" ? (
                 <>
                   Hơn 20 năm sản xuất đa dạng theo nhu cầu khách hàng với cam
@@ -36,43 +43,80 @@ export default function About() {
           </div>
         </div>
       </section>
-      <section className="bg-base-200 py-[3em] px-[2em] md:py-[4em] lg:p-[5em] container text-2xl leading-9">
-        {locale === "vi" ? (
-          <>
-            Thành lập từ năm 2001, Nhà máy Dược Phẩm Đông Nam đã trở thành{" "}
-            <strong>
-              một trong những cơ sở sản xuất dược phẩm đáng tin cậy nhất tại
-              Việt Nam.
-            </strong>{" "}
-            Tự hào về khả năng quản trị sản xuất hàng đầu, nhà máy Đông Nam tập
-            trung vào việc cung cấp các{" "}
-            <strong>sản phẩm chất lượng và đa dạng.</strong>
-          </>
-        ) : (
-          <>
-            Established in 2001, Đông Nam Pharmaceutical Factory has become{" "}
-            <strong>
-              one of the most reliable pharmaceutical manufacturing facilities
-              in Vietnam.
-            </strong>{" "}
-            Proud of our top-notch manufacturing management, Đông Nam Factory
-            focuses on providing <strong>quality and diverse products.</strong>
-          </>
-        )}
+      <section className="hidden md:block bg-base-200  px-[5em] pt-[2em]">
+        <div className="container max-w-screen-xl mx-auto">
+          <p className="text-2xl leading-10 text-justify">
+            {" "}
+            {locale === "vi" ? (
+              <>
+                Thành lập từ năm 2001, Nhà máy Dược Phẩm Đông Nam đã trở thành{" "}
+                <strong>
+                  một trong những cơ sở sản xuất dược phẩm đáng tin cậy nhất tại
+                  Việt Nam.
+                </strong>{" "}
+                Tự hào về khả năng quản trị sản xuất hàng đầu, nhà máy Đông Nam
+                tập trung vào việc cung cấp các{" "}
+                <strong>sản phẩm chất lượng và đa dạng.</strong>
+              </>
+            ) : (
+              <>
+                Established in 2001, Đông Nam Pharmaceutical Factory has become{" "}
+                <strong>
+                  one of the most reliable pharmaceutical manufacturing
+                  facilities in Vietnam.
+                </strong>{" "}
+                Proud of our top-notch manufacturing management, Đông Nam
+                Factory focuses on providing{" "}
+                <strong>quality and diverse products.</strong>
+              </>
+            )}
+          </p>
+        </div>
       </section>
-      <section className="bg-base-200 py-[3em] px-[2em] md:py-[4em] lg:p-[5em]">
+      <section className="flex flex-col md:hidden w-full">
+        <img src="/image/factory-hero-bg.jpg" className="pb-4" />
+        <div className="flex flex-col w-full md:container px-7">
+          <p className="text-[18px] text-[#00A44E] font-bold pb-2 uppercase text-center">
+            {t("title1")}
+          </p>
+          <p className="text-justify leading-[20px] text-[16px]">
+            {locale === "vi" ? (
+              <>
+                Thành lập từ năm 2001, Nhà máy Dược Phẩm Đông Nam đã trở thành{" "}
+                <strong>
+                  một trong những cơ sở sản xuất dược phẩm đáng tin cậy nhất tại
+                  Việt Nam
+                </strong>
+                . Tự hào về khả năng quản trị sản xuất hàng đầu, nhà máy Đông
+                Nam tập trung vào việc cung cấp các{" "}
+                <strong>sản phẩm chất lượng và đa dạng</strong>.
+              </>
+            ) : (
+              <>
+                Established in 2001, Đông Nam Pharmaceutical Factory has become{" "}
+                <strong>
+                  one of the most reliable pharmaceutical manufacturing
+                  facilities in Vietnam.
+                </strong>{" "}
+                Proud of our top-notch manufacturing management, Đông Nam
+                Factory focuses on providing{" "}
+                <strong>quality and diverse products.</strong>
+              </>
+            )}
+          </p>
+        </div>
+      </section>
+      <section className="hidden md:block bg-base-200 py-[3em] px-[2em] md:py-[4em] lg:p-[5em]">
         <div className="container max-w-screen-xl mx-auto grid grid-cols-2 gap-8">
           <img
             src="/image/factory-1.jpg"
-            className="w-full h-full rounded-lg max-h-80 object-cover"
+            className="w-full h-full xl:max-h-[47vh] 2xl:max-h-[39vh] rounded-lg object-cover"
           />
           <div className="flex flex-col gap-4">
             <h1 className="text-3xl uppercase font-bold text-[#F18200]">
-              {locale === "vi"
-                ? "Quy mô và cơ sở hạ tầng"
-                : "Scale and Infrastructure"}
+              {t("title2")}
             </h1>
-            <p className="text-2xl leading-9">
+            <p className="text-2xl leading-10 text-justify">
               {locale === "vi" ? (
                 <>
                   Nhà máy sản xuất dược phẩm Đông Nam đã tuân thủ nghiêm ngặt
@@ -88,36 +132,37 @@ export default function About() {
                 <>
                   To continuously improve product quality and market position
                   worldwide, Đông Nam Pharmaceutical Factory strictly adheres to
-                  principles in GMP-WHO standards. We have heavily invested in
-                  infrastructure with an area of over 13,000 m2, including
-                  essential areas such as storage warehouses, processing zones,
-                  and the most advanced automated production lines.
+                  principles in <strong>GMP-WHO standards</strong>. We have
+                  heavily invested in infrastructure with an area of over{" "}
+                  <strong>13,000 m2</strong>, including essential areas such as
+                  storage warehouses, processing zones, and the most{" "}
+                  <strong>advanced automated production lines</strong>.
                 </>
               )}
             </p>
           </div>
         </div>
-        <div className="container max-w-screen-xl mx-auto grid grid-cols-2 gap-8 mt-32">
+        <div className="container max-w-screen-xl mx-auto grid grid-cols-2 gap-8 mt-32 ">
           <div className="flex flex-col gap-4">
             <h1 className="text-3xl uppercase font-bold text-[#00A44E]">
-              {locale === "vi" ? "Chất lượng sản xuất" : "Product Quality"}
+              {t("title3")}
             </h1>
-            <p className="text-2xl leading-9">
+            <p className="text-2xl leading-10 text-justify">
               {locale === "vi" ? (
                 <>
                   Chúng tôi chuyên sản xuất các loại sản phẩm dạng{" "}
                   <strong>
                     viên nén, viên nang cứng, viên nang mềm, viên nén bao phim
                     và thuốc bột
-                  </strong>{" "}
-                  <br />
+                  </strong>
+                  . <br />
                   <br />
                   Chúng tôi đảm bảo nguồn nguyên liệu đầu vào được lựa chọn và
                   kiểm soát nghiêm ngặt theo các{" "}
                   <strong>tiêu chuẩn quốc tế CACP, GCP, GSP.</strong> Mỗi nguyên
                   liệu sử dụng đều được qua quá trình kiểm tra chất lượng nghiêm
                   ngặt bởi phòng kiểm nghiệm đạt{" "}
-                  <strong>tiêu chuẩn GLP của Bộ Y tế.</strong>
+                  <strong>tiêu chuẩn GLP của Bộ Y tế</strong>.
                 </>
               ) : (
                 <>
@@ -125,15 +170,16 @@ export default function About() {
                   as{" "}
                   <strong>
                     tablets, hard capsules, soft capsules, film-coated tablets,
-                    and powdered medicines.
+                    and powdered medicines
                   </strong>
+                  .
                   <br />
                   <br /> We ensure that our raw materials are carefully selected
                   and strictly controlled according to{" "}
                   <strong>
-                    international standards such as GACP, GCP, and GSP.
-                  </strong>{" "}
-                  Each raw material undergoes rigorous quality testing by
+                    international standards such as GACP, GCP, and GSP
+                  </strong>
+                  . Each raw material undergoes rigorous quality testing by{" "}
                   <strong>
                     GLP-standard laboratories of the Ministry of Health
                   </strong>{" "}
@@ -144,21 +190,20 @@ export default function About() {
           </div>
           <img
             src="/image/factory-2.jpg"
-            className="w-full h-full rounded-lg max-h-80 object-cover"
+            className="w-full h-full rounded-lg xl:max-h-[58vh] 2xl:max-h-[47vh]   object-cover"
           />
         </div>
         <div className="container max-w-screen-xl mx-auto grid grid-cols-2 gap-8 mt-32">
           <img
             src="/image/factory-3.jpg"
-            className="w-full h-full rounded-lg max-h-80 object-cover"
+            className="w-full h-full rounded-lg
+            xl:max-h-[44vh] 2xl:max-h-[36vh]    object-fill"
           />
           <div className="flex flex-col gap-4">
             <h1 className="text-3xl uppercase font-bold text-[#F18200]">
-              {locale === "vi"
-                ? "Hiệu quả vượt trội"
-                : "Superior Effectiveness"}
+              {t("title4")}
             </h1>
-            <p className="text-2xl leading-9">
+            <p className="text-2xl leading-[60px] text-justify pt-2">
               {locale === "vi" ? (
                 <>
                   Nhờ vào sự đầu tư mạnh mẽ vào nghiên cứu và phát triển, các
@@ -166,8 +211,9 @@ export default function About() {
                   được các{" "}
                   <strong>
                     đại lý phân phối thuốc uy tín trên toàn quốc tin tưởng đặt
-                    hàng.
+                    hàng
                   </strong>
+                  .
                 </>
               ) : (
                 <>
@@ -176,12 +222,130 @@ export default function About() {
                   trusted by{" "}
                   <strong>
                     reputable pharmaceutical distributors nationwide to place
-                    orders.
+                    orders
                   </strong>
+                  .
                 </>
               )}
             </p>
           </div>
+        </div>
+      </section>
+      <section className="flex flex-col md:hidden w-full">
+        <div className="container">
+          <img src="/image/factory-1.jpg" className="my-4  rounded-xl" />
+        </div>
+        <div className="flex flex-col w-full md:container px-7">
+          <p className="text-[18px] text-center text-[#F18200] font-bold pb-2 uppercase">
+            {t("title2")}
+          </p>
+          <p className="text-justify leading-[20px] text-[16px]">
+            {locale === "vi" ? (
+              <>
+                Nhà máy sản xuất dược phẩm Đông Nam đã tuân thủ nghiêm ngặt các
+                nguyên tắc trong <strong>tiêu chuẩn GMP – WHO.</strong> Chúng
+                tôi đã đầu tư mạnh mẽ vào cơ sở hạ tầng với diện tích lên đến
+                hơn <strong>13,000 m2</strong> với đầy đủ các chức năng, bao gồm
+                các khu vực quan trọng như kho bảo quản, khu vực xử lý, và hệ
+                thống{" "}
+                <strong>dây chuyền sản xuất theo công nghệ tự động</strong> tiên
+                tiến nhất.
+              </>
+            ) : (
+              <>
+                To continuously improve product quality and market position
+                worldwide, Đông Nam Pharmaceutical Factory strictly adheres to
+                principles in <strong>GMP-WHO standards</strong>. We have
+                heavily invested in infrastructure with an area of over{" "}
+                <strong>13,000 m2</strong>, including essential areas such as
+                storage warehouses, processing zones, and the most{" "}
+                <strong>advanced automated production lines</strong>.
+              </>
+            )}
+          </p>
+        </div>
+      </section>
+      <section className="flex flex-col md:hidden w-full ">
+        <div className="container">
+          <img src="/image/factory-2.jpg" className="my-4  rounded-xl" />
+        </div>
+        <div className="flex flex-col w-full md:container px-7">
+          <p className="text-[18px] text-center text-[#00A44E] font-bold pb-2 uppercase">
+            {t("title3")}
+          </p>
+          <p className="text-justify leading-[20px] text-[16px]">
+            {locale === "vi" ? (
+              <>
+                Chúng tôi chuyên sản xuất các loại sản phẩm dạng{" "}
+                <strong>
+                  viên nén, viên nang cứng, viên nang mềm, viên nén bao phim và
+                  thuốc bột
+                </strong>
+                . <br />
+                <br />
+                Chúng tôi đảm bảo nguồn nguyên liệu đầu vào được lựa chọn và
+                kiểm soát nghiêm ngặt theo các{" "}
+                <strong>tiêu chuẩn quốc tế CACP, GCP, GSP.</strong> Mỗi nguyên
+                liệu sử dụng đều được qua quá trình kiểm tra chất lượng nghiêm
+                ngặt bởi phòng kiểm nghiệm đạt{" "}
+                <strong>tiêu chuẩn GLP của Bộ Y tế</strong>.
+              </>
+            ) : (
+              <>
+                We specialize in manufacturing various types of products such as{" "}
+                <strong>
+                  tablets, hard capsules, soft capsules, film-coated tablets,
+                  and powdered medicines
+                </strong>
+                .
+                <br />
+                <br /> We ensure that our raw materials are carefully selected
+                and strictly controlled according to{" "}
+                <strong>
+                  international standards such as GACP, GCP, and GSP
+                </strong>
+                . Each raw material undergoes rigorous quality testing by{" "}
+                <strong>
+                  GLP-standard laboratories of the Ministry of Health
+                </strong>{" "}
+                to ensure safety for health.
+              </>
+            )}
+          </p>
+        </div>
+      </section>
+      <section className="flex flex-col md:hidden w-full mb-8 ">
+        <div className="container">
+          <img src="/image/factory-3.jpg" className="my-4  rounded-xl" />
+        </div>
+        <div className="flex flex-col w-full md:container px-7">
+          <p className="text-[18px] text-center text-[#F18200] font-bold pb-2 uppercase">
+            {t("title4")}
+          </p>
+          <p className="text-justify leading-[20px] text-[16px]">
+            {locale === "vi" ? (
+              <>
+                Nhờ vào sự đầu tư mạnh mẽ vào nghiên cứu và phát triển, các sản
+                phẩm của chúng tôi luôn đạt được hiệu quả vượt trội và được các{" "}
+                <strong>
+                  đại lý phân phối thuốc uy tín trên toàn quốc tin tưởng đặt
+                  hàng
+                </strong>
+                .
+              </>
+            ) : (
+              <>
+                Thanks to strong investment in research and development, our
+                products consistently achieve superior effectiveness and are
+                trusted by{" "}
+                <strong>
+                  reputable pharmaceutical distributors nationwide to place
+                  orders
+                </strong>
+                .
+              </>
+            )}
+          </p>
         </div>
       </section>
     </main>
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
   return (
     <footer className="relative flex flex-col justify-start items-start md:py-4 md:px-8 px-4 pt-4 bg-[#F18200] text-white">
       <aside className="flex flex-col justify-start items-start">
-        <p className="font-bold md:mt-4 text-white text-justify md:text-center capitalize text-[20px] leading-[22px] md:text-2xl xl:text-3xl">
+        <p className="font-bold md:mt-4 text-white text-justify md:text-center capitalize text-[20px] leading-[22px] md:text-2xl xl:text-3xl md:pl-2">
           {locale === "vi"
             ? "Công ty cổ phần sản xuất thương mại dược phẩm đông nam"
             : "Dong Nam Pharmaceutical Trading and Production Joint Stock Company"}
@@ -16,32 +16,35 @@ const Footer = () => {
         <p className="text-white text-justify md:text-center md:mt-4 text-[14px] md:text-2xl xl:text-3xl">
           <img
             src="/image/footer-1.png"
-            className="w-8 h-8 inline-block md:mr-2"
+            className="w-8 h-8 xl:h-12 xl:w-12 inline-block md:mr-2"
           />
           {locale === "vi" ? (
             <>
-              <strong>Trụ sở:</strong> Lô 2A Đường 1A, KCN Tân Tạo, Q.Bình Tân,
-              TP.HCM
+              <strong className="hidden md:inline">Trụ sở:</strong> Lô 2A Đường
+              1A, KCN Tân Tạo, Q.Bình Tân, TP.HCM
             </>
           ) : (
             <>
-              <strong>Headquarters:</strong> Lot 2A, Street 1A, Tan Tao
-              Industrial Park, Binh Tan District, Ho Chi Minh City
+              <strong className="hidden md:inline">Headquarters:</strong> Lot
+              2A, Street 1A, Tan Tao Industrial Park, Binh Tan District, Ho Chi
+              Minh City
             </>
           )}
         </p>
         <p className="text-white text-justify md:text-center text-[14px] md:text-2xl xl:text-3xl">
           <img
             src="/image/footer-2.png"
-            className="w-8 h-8 inline-block md:mr-2"
+            className="w-8 h-8 xl:h-12 xl:w-12 inline-block md:mr-2"
           />
           {locale === "vi" ? (
             <>
-              <strong>Liên hệ:</strong> (+84 283) 7541 748 - (+84 283) 7541 749
+              <strong className="hidden md:inline">Liên hệ:</strong> (+84 283)
+              7541 748 - (+84 283) 7541 749
             </>
           ) : (
             <>
-              <strong>Contact:</strong> (+84 283) 7541 748 - (+84 283) 754 1749
+              <strong className="hidden md:inline">Contact:</strong> (+84 283)
+              7541 748 - (+84 283) 754 1749
             </>
           )}
         </p>
@@ -49,14 +52,14 @@ const Footer = () => {
           <img
             src="/image/footer-3.png"
             className="w-8 h-8 inline-block md:mr-2"
-          />
-          <strong>Email:</strong> kinhdoanh@dongnampharma.vn
+          />{" "}
+          kinhdoanh@dongnampharma.vn
         </p>
         <div className="text-white md:text-2xl xl:text-3xl inline    w-full">
           <div className="hidden md:block xl:h-[10vh] 2xl:h-[12vh]">
             <img
               src="/image/footer-3.png"
-              className="w-10 h-10 inline-block mr-2"
+              className="w-8 h-8 xl:h-12 xl:w-12 inline-block mr-2"
             />
             <strong>Email:</strong> kinhdoanh@dongnampharma.vn
           </div>
