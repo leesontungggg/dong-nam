4df8a4528f44c7da47d0f22722d0e585d34696a5
Bao <baoohg@gmail.com>
2024-05-17 01:23:13 +0700

    fix homepage

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -37,9 +37,9 @@ export default function Root() {
           <div
             className={`${
               locale === "en"
-                ? "md:w-[58vw] md:h-[290px] 2xl:w-[44vw] 2xl:h-[44vh]"
-                : "md:w-[50vw] md:h-[310px] 2xl:w-[42vw] 2xl:h-[44vh]"
-            } flex flex-col md:gap-4 xl:gap-6 h-fit xl:w-[48vw] xl:h-[43vh] justify-center bg-white rounded-[60px] px-8`}
+                ? "md:w-[58vw] md:h-[340px] 2xl:w-[44vw] 2xl:h-[44vh]"
+                : "md:w-[50vw] md:h-[360px] 2xl:w-[42vw] 2xl:h-[44vh]"
+            } flex flex-col md:gap-4 xl:gap-6 h-fit xl:w-[48vw] xl:h-[46vh] justify-center bg-white rounded-[60px] px-8`}
           >
             <h1
               className={`md:text-3xl xl:text-3xl xl:max-2xl:pt-4 text-[#00A44E] font-bold text-center mx-auto load-hidden animate-fade-in uppercase`}
